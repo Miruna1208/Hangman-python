3ba59eee2c754bf917dad596cf43197b4e904813
Miruna1208 <miruna.stanescu12@gmail.com>
2025-08-26 16:11:07 +0300

    Game done with basic words

--- hangman.py
+++ hangman.py
@@ -8,9 +8,101 @@ word_list = [
     "mountain", "river", "forest", "desert", "ocean"
 ]
 
-secret_word = random.choice(word_list)
+hangman = [
+        r"""
+  -----
+  |   |
+      |
+      |
+      |
+      |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+      |
+      |
+      |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+  |   |
+  |   |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+/ |   |
+  |   |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+/ | \ |
+  |   |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+/ | \ |
+  |   |
+ ---  |
+/     |
+|     |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+/ | \ |
+  |   |
+ ---  |
+/   \ |
+|   | |
+      |
+-------
+""",
+    ]
 
-print("Your secret word: ")
+secret_word = random.choice(word_list)
 
 secret_word_letters = [];
 secret_word_lines = [];
@@ -18,21 +110,37 @@ for char in secret_word:
     secret_word_letters.append(char)
     secret_word_lines.append("_")
 
-print(''.join(secret_word_lines))
+count_mistakes = 0;
+used_letters = [];
 
-word_found = 0
-
-while(not word_found):
-    print("Type your guess: ")
+while(True):
+    print("Type your guess(used letters: "+ ' '.join(used_letters) + ")")
+    print(''.join(secret_word_lines))
     letter = input()
-    if letter in secret_word:
-        for i in range(len(secret_word)):
-            if secret_word_letters[i] == letter:
-                secret_word_lines[i] = letter
-        print(''.join(secret_word_lines))
+    if letter not in used_letters:
+        if letter in secret_word:
+            for i in range(len(secret_word)):
+                if secret_word_letters[i] == letter:
+                    secret_word_lines[i] = letter
+            print(''.join(secret_word_lines))
+            if "_" not in secret_word_lines:
+                print("You won! :)")
+                break
+        elif letter not in secret_word:
+            count_mistakes += 1
+            if count_mistakes != 7:
+                print(hangman[count_mistakes])
+            else:
+                print("You lost :|")
+                print("The word is: " + secret_word)
+                break
+        used_letters.append(letter)
+    else:
+        print("You already used this letter! ")
+        
 
-    elif letter not in secret_word:
-        print("not ok")
+        
+        
 
 
 
